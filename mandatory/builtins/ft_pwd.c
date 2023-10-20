2985aa27bcd6a9161a9ba8807fb96c33120fd2cf
Caroline Kunimura Plaza <ckunimur@c2r2p5.42sp.org.br>
2023-10-20 16:19:02 -0300

    pwd

--- mandatory/builtins/ft_pwd.c
+++ mandatory/builtins/ft_pwd.c
@@ -6,25 +6,18 @@
 /*   By: ckunimur <ckunimur@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 21:06:02 by ckunimur          #+#    #+#             */
-/*   Updated: 2023/10/18 15:00:40 by ckunimur         ###   ########.fr       */
+/*   Updated: 2023/10/18 18:01:53 by ckunimur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void ft_pwd(t_data *data)
+void	ft_pwd(void)
 {
-	t_env *env_node;
+	char	current_dir[1024];
 
-	env_node = data->env_node;
-	while(env_node->next != NULL)
-	{
-		if (!ft_strncmp(env_node->var, "PWD", 4))
-		{
-			ft_printf("%s\n", env_node->value);
-			return;
-		}
-		env_node = env_node->next;
-		
-}
+	if (getcwd(current_dir, sizeof(current_dir)) != NULL)
+		printf("%s\n", current_dir);
+	else
+		perror("getcwd");
 }
