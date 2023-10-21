c98dc926b2c5e5ef3be8fb6c705fd5e3c8ea84c5
gacalaza <gacalaza@42sp.org.br>
2023-10-21 15:39:03 -0300

    norminette OK.

--- mandatory/prompt.c
+++ mandatory/prompt.c
@@ -6,7 +6,7 @@
 /*   By: gacalaza <gacalaza@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 15:55:06 by gacalaza          #+#    #+#             */
-/*   Updated: 2023/10/20 20:07:48 by gacalaza         ###   ########.fr       */
+/*   Updated: 2023/10/21 15:38:33 by gacalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,46 +17,77 @@
 // Agora 'input' contém o comando digitado pelo usuário
 // VERIFICAR LEAK DE MEMORIA??
 
-void	sub_set_path_command(t_data *data, char *tmp1, char **path, char *tmp2)
+char	*create_command_path(char *path, char *command)
 {
-		if (data->cmd[0])
-			free(data->cmd[0]);
-		data->cmd[0] = tmp2;
-		ft_clean_lst(path);
-		return ;
+	char	*tmp1;
+	char	*tmp2;
+
+	*tmp1 = ft_strjoin(path, "/");
+	if (!tmp1)
+		return (NULL);
+	tmp2 = ft_strjoin(tmp1, command);
+	free(tmp1);
+	if (!access(tmp2, F_OK) && !access(tmp2, X_OK))
+		return (tmp2);
+	free(tmp2);
+	return (NULL);
 }
 
 void	set_path_command(t_data *data)
 {
 	char	**path;
-	char	*tmp1;
-	char	*tmp2;
+	char	*command_path;
 	int		i;
 
-	i = 0;
 	path = ft_split(data->path, ':');
+	i = 0;
 	while (path[i])
 	{
-		tmp1 = ft_strjoin(path[i], "/");
-		tmp2 = ft_strjoin(tmp1, data->cmd[0]);
-		if (tmp1)
-			free(tmp1);
-		if (!access(tmp2, F_OK))
+		command_path = create_command_path(path[i], data->cmd[0]);
+		if (command_path)
 		{
-			if (!access(tmp2, X_OK))
-			{
-				if (data->cmd[0])
-					free(data->cmd[0]);
-				data->cmd[0] = tmp2;
-				ft_clean_lst(path);
-				return ;
-			}
+			if (data->cmd[0])
+				free(data->cmd[0]);
+			data->cmd[0] = command_path;
+			ft_clean_lst(path);
+			return ;
 		}
-		if (tmp2)
-			free(tmp2);
 		i++;
 	}
+	ft_clean_lst(path);
 }
+
+// void	set_path_command(t_data *data)
+// {
+// 	char	**path;
+// 	char	*tmp1;
+// 	char	*tmp2;
+// 	int		i;
+
+// 	i = 0;
+// 	path = ft_split(data->path, ':');
+// 	while (path[i])
+// 	{
+// 		tmp1 = ft_strjoin(path[i], "/");
+// 		tmp2 = ft_strjoin(tmp1, data->cmd[0]);
+// 		if (tmp1)
+// 			free(tmp1);
+// 		if (!access(tmp2, F_OK))
+// 		{
+// 			if (!access(tmp2, X_OK))
+// 			{
+// 				if (data->cmd[0])
+// 					free(data->cmd[0]);
+// 				data->cmd[0] = tmp2;
+// 				ft_clean_lst(path);
+// 				return ;
+// 			}
+// 		}
+// 		if (tmp2)
+// 			free(tmp2);
+// 		i++;
+// 	}
+// }
 
 /*
 [] pipe
