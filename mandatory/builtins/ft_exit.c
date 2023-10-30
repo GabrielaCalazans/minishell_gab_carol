3577c334a17e9aa255e6692c27c7f14f9dc45aa8
caroline.kunimura <ckunimur@42sp.org.br>
2023-10-30 18:36:13 -0300

    exit

--- mandatory/builtins/ft_exit.c
+++ mandatory/builtins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: ckunimur <ckunimur@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 21:05:26 by ckunimur          #+#    #+#             */
-/*   Updated: 2023/10/24 19:40:01 by ckunimur         ###   ########.fr       */
+/*   Updated: 2023/10/30 17:26:03 by ckunimur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,25 @@ void	ft_exit(t_data *data)
 	long err;
 
 	err = 1;
-	if (!data->cmd_args[1])
+	if (!data->cmd[1])
 	{
-		if(data->cmd_args[0])
-			err = ft_atoi(data->cmd_args[0]);
-		if((!err && (ft_strlen(data->cmd_args[0]) > 2) )|| !ft_isdigit(ft_atoi(data->cmd_args[0])))
-		{
-			if(!(ft_strlen(data->cmd_args[0]) > 19))
-			{
-				err = 2;
-				ft_printf("numeric argument is required\n");
-			}
-		}
+		ft_clear_data(data);
+		ft_printf("exit\n");
+		exit(err);
 	}
-	else
-		printf("exit\nexit: too many argments");
-	ft_clear_data(data);
-	exit(err);
+	else if (data->cmd[2])
+		printf("exit: too many argments\n");
+	else if (data->cmd[1])
+	{
+		if (ft_atoi(data->cmd[1]) == 0 && (data->cmd[1][0] != '0'))
+		{
+			err = 2;
+			ft_printf("numeric argument is required\n");
+		}
+		else if(ft_strlen(data->cmd[1]) < 19)
+			err = ft_atoi(data->cmd[1]);
+		ft_clear_data(data);
+		ft_printf("exit\n");
+		exit(err);
+	}
 }
