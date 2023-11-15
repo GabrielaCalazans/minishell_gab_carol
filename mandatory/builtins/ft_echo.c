7fe554e237218417a81e17c1aeb65169d03b1a8b
gacalaza <gacalaza@student.42.fr>
2023-11-15 15:06:11 -0300

    Adjustment in ft_echo in case of the -n flag.

--- mandatory/builtins/ft_echo.c
+++ mandatory/builtins/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: gacalaza <gacalaza@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 21:04:33 by ckunimur          #+#    #+#             */
-/*   Updated: 2023/11/14 18:50:35 by gacalaza         ###   ########.fr       */
+/*   Updated: 2023/11/15 14:48:52 by gacalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,25 @@ void	ft_echo(t_data *data)
 	int		i;
 	t_data	*temp;
 
-	i = 1;
+	i = 0;
 	temp = data;
 	printf("%s", temp->cmd_args[i]);
 	i++;
-	while (temp->cmd_args[i])
+	if (ft_array_size(data->cmd) > 1 && ft_strncmp(data->cmd[1], "-n", 3) == 0)
 	{
-		printf(" %s", temp->cmd_args[i]);
-		i++;
+		while (temp->cmd_args[i])
+		{
+			printf(" %s", temp->cmd_args[i]);
+			i++;
+		}
 	}
-	printf("\n");
+	else
+	{
+		while (temp->cmd_args[i])
+		{
+			printf(" %s", temp->cmd_args[i]);
+			i++;
+		}
+		printf("\n");
+	}
 }
