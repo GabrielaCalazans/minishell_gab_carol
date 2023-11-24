fb6d093d4e43cc9a095122b627a0b74e6aeaecc4
caroline.kunimura <ckunimur@42sp.org.br>
2023-11-24 19:14:19 -0300

    close fd feito

--- mandatory/execution.c
+++ mandatory/execution.c
@@ -6,7 +6,7 @@
 /*   By: ckunimur <ckunimur@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 16:55:22 by gacalaza          #+#    #+#             */
-/*   Updated: 2023/11/22 16:17:03 by ckunimur         ###   ########.fr       */
+/*   Updated: 2023/11/24 16:45:28 by ckunimur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@ void	execution(t_data *data)
 {
 	int	pid[3];
 	int	status;
-	int ord = 0;
-	int i = 0;
+	int	ord;
+	int	i;
 
+	ord = 0;
+	i = 0;
 	data->n_cmd = 3;
 	data->fd = ft_calloc(data->n_cmd * 2, sizeof(int));
 	while (i < data->n_cmd * 2 - 1)
@@ -30,19 +32,17 @@ void	execution(t_data *data)
 	status = 0;
 	while (i < 3)
 	{
-		// if (i == 0) 
-		// 	data->cmd = (char *[]) {"/usr/bin/ls", NULL};
-		// else if (i == 1)
-		// 	data->cmd = (char *[]) {"/usr/bin/wc", "-c", NULL};
-		// else 	
-		// 	data->cmd = (char *[]) {"/usr/bin/tr", "6", "9", NULL};
+		if (i == 0) 
+			data->cmd = (char *[]) {"/usr/bin/ls", NULL};
+		else if (i == 1)
+			data->cmd = (char *[]) {"/usr/bin/wc", "-c", NULL};
+		else 	
+			data->cmd = (char *[]) {"/usr/bin/tr", "6", "9", NULL};
 		pid[i] = fork();
 		if (pid[i] == 0)
 		{
 			int bkp = dup(1);
-			printf("%i\n", i);
-			// set_path_command(data);
-			printf("%s\n", data->cmd[0]);
+			set_path_command(data);
 			dup_pipe(ord, data);
 			execve(data->cmd[0], data->cmd, data->env);
 			dup2(bkp, 1);
@@ -52,10 +52,7 @@ void	execution(t_data *data)
 		ord++;
 		i++;
 	}
-	close(data->fd[0]);
-	close(data->fd[1]);
-	close(data->fd[2]);
-	close(data->fd[3]);
+	close_fd(data, data->n_cmd * 2);
 	for (int y = 0; y < data->n_cmd; y++) {
 		waitpid(pid[y], &status, 0);
 	}
@@ -64,30 +61,29 @@ void	execution(t_data *data)
 void	dup_pipe(int ord, t_data *data)
 {
 	
-	if (ord == 0) {
-		printf("primeiro pipe\n");
+	if (ord == 0)
+	{
 		dup2(data->fd[1], 1);
-		close(data->fd[0]);
-		close(data->fd[1]);
-		close(data->fd[2]);
-		close(data->fd[3]);
+		close_fd(data, data->n_cmd * 2);
 	}
-	else if (ord == data->n_cmd - 1) {
-		printf("segundo pipe\n");
+	else if (ord == data->n_cmd - 1)
+	{
 		dup2(data->fd[2], 0);
-		close(data->fd[0]);
-		close(data->fd[1]);
-		close(data->fd[2]);
-		close(data->fd[3]);
+		close_fd(data, data->n_cmd * 2);
 	}
 	else
 	{
-		printf("else pipe\n");	
 		dup2(data->fd[0], 0);
 		dup2(data->fd[3], 1);
-		close(data->fd[0]);
-		close(data->fd[1]);
-		close(data->fd[2]);
-		close(data->fd[3]);
+		close_fd(data, data->n_cmd * 2);
 	}
+}
+
+void	close_fd(t_data *data, int n_fd)
+{
+	int	i;
+
+	i = 0;
+	while (i <= n_fd)
+		close(data->fd[i++]);
 }
